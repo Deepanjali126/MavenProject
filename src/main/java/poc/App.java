42f9fb2890301d54eaf6743f227d57c661912f30
Deepanjali  Subhdarshini <Deepanjali.Subhdarshini@Ltts.com>
2020-02-20 16:00:37 +0530

    NEw project

---
+++ src/main/java/poc/App.java
@@ -0,0 +1,19 @@
+package poc;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String a = "larsen";
+		String b = "toubr";
+		String ab = a+b;
+		
+		System.out.print("ab :"+ab);
+    }
+}
+
